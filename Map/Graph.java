a997738e927e3635decff2b75933dbb49a8eebbe
Aidan <55853976+aidanLarock@users.noreply.github.com>
2022-02-04 17:17:19 -0500

    Updated Map class
    added methods and commented code

--- Map/Graph.java
+++ Map/Graph.java
@@ -1,39 +1,57 @@
 package Map;
 
 /**
- * The class Graph stores and loads the graph of the
- * Map using an adjacency matrix. Can also get a
- * Road Segement with a start and a end location.
+ * The class Graph stores and loads the graph of the 
+ * Map using an adjacency matrix. Can also get a 
+ * Road Segement with a start and a end location. 
  * 
  * @author Aidan Larock 6186076, Michael Wisniewski 6402176
  * @version 2 Feb 2022
- * @see Main
- * @see Map
+ * @see Main  
+ * @see Map 
  */
 public class Graph {
 
-  public Intersection[][] inter;
+  private Intersection[][] intersection;
+
+  private RoadSegment road;
 
   Integer numRoads;
 
   Integer numInter;
 
   /**
-   * Loads and stores the adjacency matrix used for the graph.
-   * 
-   * @param inter The adjacency matrix of type Integer.
+   * Loads and stores the adjacency matrix used for the graph. 
+   * @param inter The adjacency matrix of type Integer. 
    */
-  public void loadMap(Intersection[][] inter) {
-
+  public void loadMap(Intersection[][] intersection) {
+    
   }
 
   /**
-   * A getter function designed to get the road segment(s) from the
-   * starting intersection and ending intersection.
-   * 
+   * Gets the map that is stored in memory which is comprised of intersections and road segments.
+   * @return the graph in an adjacency matrix
+   */
+  public Intersection[][] getMap(){
+    return null;
+  }
+
+  /**
+   * Adds a road (edges) between intersections (graph nodes)
+   * @param start from intersection
+   * @param end to intersection
+   * @param road RoadSegment to add
+   */
+  private void addRoad(Intersection start, Intersection end, RoadSegment road){
+    
+  }
+ 
+  /**
+   * A getter function designed to get the road segment(s) from the 
+   * starting intersection and ending intersection. 
    * @param start the starting intersection
-   * @param end   the ending intersection
-   * @return the road segment from start to end.
+   * @param end the ending intersection 
+   * @return the road segment from start to end. 
    */
   public RoadSegment getRoad(Intersection start, Intersection end) {
     return null;
--- Map/RoadSegment.java
+++ Map/RoadSegment.java
@@ -1,5 +1,13 @@
 package Map;
 
+
+/**
+ * The class RoadSegment stores the information of a road (edge between intersections).
+ * The class holds information about roads such as length of a road, density of a road, and the number of lanes
+ * @author Aidan Larock 6186076, Michael Wisniewski 6402176
+ * @version 2 Feb 2022 
+ * @see Map 
+ */
 public class RoadSegment {
 
   private Integer length;
@@ -8,17 +16,26 @@ public class RoadSegment {
 
   private Integer numLanes;
 
-  public void getWeight() {
-  }
-
+  /**
+   * This method gets the length of a road segment
+   * @return road segment length
+   */
   Integer getLength() {
     return null;
   }
 
+  /**
+   * This method gets the number of cars that are on the road segment
+   * @return vehicle density
+   */
   Integer getDensity() {
     return null;
   }
 
+  /**
+   * This method gets the number of lanes a road segment has
+   * @return number of lanes 
+   */
   Integer getNumberLanes() {
     return null;
   }
