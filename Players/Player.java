372bfa5109671ac34fd7d375e1daca9da8051342
Mikes <35347523+akaJengo@users.noreply.github.com>
2022-02-04 17:25:15 -0500

    Update Player.java

--- Players/Player.java
+++ Players/Player.java
@@ -3,7 +3,8 @@ package Players;
 import Vehicle.Vehicle;
 
 /**
- * This class operates the player movement.
+ * This class player is a type of vehicle object which allows for
+ * a user to interact with and move a vehicle object.
  * 
  * 
  * @author Aidan Larock 6186076, Michael Wisniewski 6402176
@@ -15,12 +16,22 @@ public class Player {
 
   private Vehicle type;
 
+  /**
+   * Allows the player to request information
+   * about the veicles surroundings.
+   */
   public void looking() {
   }
 
+  /**
+   * Allows the player to move a vehicle.
+   */
   public void moving() {
   }
 
+  /**
+   * Asks the player to confirm the players movemment.
+   */
   public void confirm() {
   }
 
